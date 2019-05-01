7551cf313561e1c7d592132f35804141e66f21e4
Rafael Mansilha <rafael.mansilha@gmail.com>
2019-05-01 10:40:58 -0300

    - Adicionar FOSUserBundle - Adicionar SonataAdmin - Criar entidade User - Adicionar swiftmailer

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,8 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    Sonata\CoreBundle\SonataCoreBundle::class => ['all' => true],
+    Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
 ];
